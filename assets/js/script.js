a322f987290ad74769a249cc902ea3b93677d128
McKendree Strommer <mckendree.strommer@gmail.com>
2021-06-22 10:52:38 -0700

    moved functions around and added comments to make code more clear

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,7 @@ const setHeaderImg = (imgEl) => {
 
 setHeaderImg(headerImg);
 
+//get token and perform auto-fetches
 const getToken = () => {
   fetch('https://api.petfinder.com/v2/oauth2/token', {
     body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${apiSecret}`,
@@ -40,8 +41,19 @@ const getToken = () => {
     });
 };
 
-//get token and perform auto-fetches
 getToken();
+
+function checkOnReload() {
+  const ifSearchUrl = new URL(window.location.href);
+  console.log(ifSearchUrl.search);
+  console.log(ifSearchUrl.searchParams.toString());
+  if (ifSearchUrl.search.length) {
+    fillSearch(
+      `https://api.petfinder.com/v2/animals?${ifSearchUrl.searchParams.toString()}`
+    );
+  }
+}
+
 //fill the breed dropdown list from a call to the server (over 250 items)
 function fillBreedList() {
   fetch('https://api.petfinder.com/v2/types/dog/breeds', {
@@ -72,17 +84,107 @@ function fillBreedList() {
     });
 }
 
-function checkOnReload() {
-  const ifSearchUrl = new URL(window.location.href);
-  console.log(ifSearchUrl.search);
-  console.log(ifSearchUrl.searchParams.toString());
-  if (ifSearchUrl.search.length) {
-    fillSearch(
-      `https://api.petfinder.com/v2/animals?${ifSearchUrl.searchParams.toString()}`
-    );
+//on click, fills the search area below the search button with 20 results
+//performs an authority check to receive a token, then performs the fetch including the zip code.
+const startSearch = () => {
+  //if there's an error msg, remove it on next search
+  while (document.getElementById('error-msg')) {
+    console.log('this means the error is removed');
+    document.getElementById('error-msg').remove();
   }
-}
+  //build api url for request from user's options
+  const zip = document.getElementById('zip').value;
+  const breed = document.getElementById('breed').value;
+  const housingReqs = Array.from(
+    document.querySelectorAll('input[type="checkbox"]:checked')
+  );
 
+  removeUrlCheckboxes();
+  function removeUrlCheckboxes() {
+    if (apiUrl.searchParams.has('children')) {
+      apiUrl.searchParams.delete('children');
+      console.log(apiUrl.searchParams.has('children'));
+    }
+    if (apiUrl.searchParams.has('dogs')) {
+      apiUrl.searchParams.delete('dogs');
+    }
+    if (apiUrl.searchParams.has('cats')) {
+      apiUrl.searchParams.delete('cats');
+    }
+    if (apiUrl.searchParams.has('spayed_neutered')) {
+      apiUrl.searchParams.delete('spayed_neutered');
+    }
+    if (apiUrl.searchParams.has('special_needs')) {
+      apiUrl.searchParams.delete('special_needs');
+    }
+
+    if (apiUrl.searchParams.has('location')) {
+      apiUrl.searchParams.delete('location');
+    }
+    if (apiUrl.searchParams.has('breed')) {
+      apiUrl.searchParams.delete('breed');
+    }
+  }
+
+  console.log(apiUrl);
+  console.log(apiUrl.searchParams.has('location'));
+
+  if (zip && !apiUrl.searchParams.has('location')) {
+    apiUrl.searchParams.append('location', zip);
+  }
+  if (breed) {
+    apiUrl.searchParams.append('breed', breed);
+  }
+  if (housingReqs.length) {
+    console.log(apiUrl.searchParams.has('children'));
+    for (let i = 0; i < housingReqs.length; i++) {
+      if (
+        housingReqs[i].dataset.input === 'good-with-kids' &&
+        !apiUrl.searchParams.has('children')
+      ) {
+        apiUrl.searchParams.append('children', 'true');
+        console.log(apiUrl.searchParams.has('children'));
+      }
+      if (
+        housingReqs[i].dataset.input === 'good-with-dogs' &&
+        !apiUrl.searchParams.has('dogs')
+      ) {
+        apiUrl.searchParams.append('dogs', 'true');
+      }
+      if (
+        housingReqs[i].dataset.input === 'good-with-cats' &&
+        !apiUrl.searchParams.has('cats')
+      ) {
+        apiUrl.searchParams.append('cats', 'true');
+      }
+      if (
+        housingReqs[i].dataset.input === 'spayed-neutered' &&
+        !apiUrl.searchParams.has('spayed_neutered')
+      ) {
+        apiUrl.searchParams.append('spayed_neutered', 'true');
+      }
+      if (
+        housingReqs[i].dataset.input === 'special-needs' &&
+        !apiUrl.searchParams.has('special_needs')
+      ) {
+        apiUrl.searchParams.append('special_needs', 'true');
+      }
+    }
+  }
+
+  console.log(typeof apiUrl);
+  console.log('the constructed url is: ------' + apiUrl);
+  console.log('the search params are: ' + apiUrl.search.substring(1));
+  window.history.replaceState(
+    {},
+    '',
+    `${location.pathname}?${apiUrl.search.substring(1)}`
+  );
+
+  fillSearch(apiUrl);
+};
+
+//handle placing items from search in the DOM
 function fillSearch(url) {
   fetch(url, {
     headers: {
@@ -225,105 +327,6 @@ function fillSearch(url) {
       }
     });
 }
-//on click, fills the search area below the search button with 20 results
-//performs an authority check to receive a token, then performs the fetch including the zip code.
-const startSearch = () => {
-  //if there's an error msg, remove it on next search
-  while (document.getElementById('error-msg')) {
-    console.log('this means the error is removed');
-    document.getElementById('error-msg').remove();
-  }
-  //build api url for request from user's options
-  const zip = document.getElementById('zip').value;
-  const breed = document.getElementById('breed').value;
-  const housingReqs = Array.from(
-    document.querySelectorAll('input[type="checkbox"]:checked')
-  );
-
-  removeUrlCheckboxes();
-  function removeUrlCheckboxes() {
-    if (apiUrl.searchParams.has('children')) {
-      apiUrl.searchParams.delete('children');
-      console.log(apiUrl.searchParams.has('children'));
-    }
-    if (apiUrl.searchParams.has('dogs')) {
-      apiUrl.searchParams.delete('dogs');
-    }
-    if (apiUrl.searchParams.has('cats')) {
-      apiUrl.searchParams.delete('cats');
-    }
-    if (apiUrl.searchParams.has('spayed_neutered')) {
-      apiUrl.searchParams.delete('spayed_neutered');
-    }
-    if (apiUrl.searchParams.has('special_needs')) {
-      apiUrl.searchParams.delete('special_needs');
-    }
-
-    if (apiUrl.searchParams.has('location')) {
-      apiUrl.searchParams.delete('location');
-    }
-    if (apiUrl.searchParams.has('breed')) {
-      apiUrl.searchParams.delete('breed');
-    }
-  }
-
-  console.log(apiUrl);
-  console.log(apiUrl.searchParams.has('location'));
-
-  if (zip && !apiUrl.searchParams.has('location')) {
-    apiUrl.searchParams.append('location', zip);
-  }
-  if (breed) {
-    apiUrl.searchParams.append('breed', breed);
-  }
-  if (housingReqs.length) {
-    console.log(apiUrl.searchParams.has('children'));
-    for (let i = 0; i < housingReqs.length; i++) {
-      if (
-        housingReqs[i].dataset.input === 'good-with-kids' &&
-        !apiUrl.searchParams.has('children')
-      ) {
-        apiUrl.searchParams.append('children', 'true');
-        console.log(apiUrl.searchParams.has('children'));
-      }
-      if (
-        housingReqs[i].dataset.input === 'good-with-dogs' &&
-        !apiUrl.searchParams.has('dogs')
-      ) {
-        apiUrl.searchParams.append('dogs', 'true');
-      }
-      if (
-        housingReqs[i].dataset.input === 'good-with-cats' &&
-        !apiUrl.searchParams.has('cats')
-      ) {
-        apiUrl.searchParams.append('cats', 'true');
-      }
-      if (
-        housingReqs[i].dataset.input === 'spayed-neutered' &&
-        !apiUrl.searchParams.has('spayed_neutered')
-      ) {
-        apiUrl.searchParams.append('spayed_neutered', 'true');
-      }
-      if (
-        housingReqs[i].dataset.input === 'special-needs' &&
-        !apiUrl.searchParams.has('special_needs')
-      ) {
-        apiUrl.searchParams.append('special_needs', 'true');
-      }
-    }
-  }
-
-  console.log(typeof apiUrl);
-  console.log('the constructed url is: ------' + apiUrl);
-  console.log('the search params are: ' + apiUrl.search.substring(1));
-  window.history.replaceState(
-    {},
-    '',
-    `${location.pathname}?${apiUrl.search.substring(1)}`
-  );
-
-  fillSearch(apiUrl);
-};
 
 document.getElementById('search-btn').addEventListener('click', startSearch);
 
